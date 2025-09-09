8ed0ab5951931a781a6c63aeb832e8db43cfa088
PalomaGit <pocapin995@g.educaand.es>
2025-09-09 18:08:18 +0200

    Actualización de código

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Adivina el número</title>
+  <title>adivinar-numero</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
+
+
+<header>
+  <h1>Adivina el número (PHP)</h1>
+  <h2>Certificaciones de Profesionalidad</h2>
+</header>
+<br>
+
 <body>
   <h1>Adivina el número</h1>
 
@@ -13,56 +23,60 @@
   <p>¡Ánimo!</p>
 
   <?php
-    $muestraFormulario = true;
+  $muestraFormulario = true;
 
-    if (!isset($_POST["numeroIntroducido"])) { // Primera carga de página
-      $numeroMisterioso = rand(1, 100);
-      $oportunidades = 5;
-    } else { // Segunda y sucesivas cargas de página
-      $numeroIntroducido = $_POST["numeroIntroducido"];
-      $numeroMisterioso = $_POST["numeroMisterioso"];
-      $oportunidades = $_POST["oportunidades"];
-      $oportunidades--;
+  if (!isset($_POST["numeroIntroducido"])) { // Primera carga de página
+    $numeroMisterioso = rand(1, 100);
+    $oportunidades = 5;
+  } else { // Segunda y sucesivas cargas de página
+    $numeroIntroducido = $_POST["numeroIntroducido"];
+    $numeroMisterioso = $_POST["numeroMisterioso"];
+    $oportunidades = $_POST["oportunidades"];
+    $oportunidades--;
 
-      if ($numeroIntroducido == $numeroMisterioso) {
-        echo "<p>🎉🎉🎉¡Enhorabuena! ¡Has acertado el número!🎉🎉🎉</p>";
-        $muestraFormulario = false;
-      }
-
-      if (($oportunidades == 0) && ($numeroIntroducido != $numeroMisterioso)) {
-        echo "<p>😿😿😿¡Lo siento! ¡Has perdido!😿😿😿</p>";
-        echo "<p>El número que estaba pensando es el $numeroMisterioso.</p>";
-        $muestraFormulario = false;
-      }
-
-      if (($numeroMisterioso < $numeroIntroducido) && ($oportunidades > 0)) {
-        echo "<p>El número que estoy pensando 🤔 es menor que $numeroIntroducido.</p>";
-        echo "<p>Te quedan $oportunidades oportunidades.</p>";
-      }
-
-      if (($numeroMisterioso > $numeroIntroducido) && ($oportunidades > 0)) {
-        echo "<p>El número que estoy pensando 🤔 es mayor que $numeroIntroducido.</p>";
-        echo "<p>Te quedan $oportunidades oportunidades.</p>";
-      }
+    if ($numeroIntroducido == $numeroMisterioso) {
+      echo "<p>🎉🎉🎉¡Enhorabuena! ¡Has acertado el número!🎉🎉🎉</p>";
+      $muestraFormulario = false;
     }
+
+    if (($oportunidades == 0) && ($numeroIntroducido != $numeroMisterioso)) {
+      echo "<p>😿😿😿¡Lo siento! ¡Has perdido!😿😿😿</p>";
+      echo "<p>El número que estaba pensando es el $numeroMisterioso.</p>";
+      $muestraFormulario = false;
+    }
+
+    if (($numeroMisterioso < $numeroIntroducido) && ($oportunidades > 0)) {
+      echo "<p>El número que estoy pensando 🤔 es menor que $numeroIntroducido.</p>";
+      echo "<p>Te quedan $oportunidades oportunidades.</p>";
+    }
+
+    if (($numeroMisterioso > $numeroIntroducido) && ($oportunidades > 0)) {
+      echo "<p>El número que estoy pensando 🤔 es mayor que $numeroIntroducido.</p>";
+      echo "<p>Te quedan $oportunidades oportunidades.</p>";
+    }
+  }
   ?>
 
   <?php
-    if ($muestraFormulario) {
-  ?>
-      <form action="index.php" method="post">
-        <label for="numeroIntroducido">Introduce un número:</label>
-        <input type="number" name="numeroIntroducido" id="numeroIntroducido" min="1" max="100" autofocus>
-        <input type="hidden" name="numeroMisterioso" value="<?= $numeroMisterioso ?>">
-        <input type="hidden" name="oportunidades" value="<?= $oportunidades ?>">
-        <input type="submit" value="Aceptar">
-      </form>
-  <?php
-    } else {
-  ?>
+  if ($muestraFormulario) {
+    ?>
+    <form action="index.php" method="post">
+      <label for="numeroIntroducido">Introduce un número:</label>
+      <input type="number" name="numeroIntroducido" id="numeroIntroducido" min="1" max="100" autofocus>
+      <input type="hidden" name="numeroMisterioso" value="<?= $numeroMisterioso ?>">
+      <input type="hidden" name="oportunidades" value="<?= $oportunidades ?>">
+      <input type="submit" value="Aceptar">
+    </form>
+    <?php
+  } else {
+    ?>
     <a href="index.php"><button>Volver a jugar</button></a>
-  <?php
-    }
+    <?php
+  }
   ?>
 </body>
+<footer>
+  <h2>© Paloma Ocaña Piña</h2>
+</footer>
+
 </html>
